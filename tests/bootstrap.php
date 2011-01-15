25a925d4ce8f7cfc6e36aa4d46e3120271e093e9
Shawn Stratton <sstratton@php.net>
2011-01-15 18:00:11 -0500

    Created some documentation and started changing the way we'll be distributing base components (including dependencies)

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,10 +1,13 @@
 <?php
-
+//Set Error Reporting and required PHP Settings.  We want tests to be verbose as
+//possible.
 error_reporting(E_ALL | E_STRICT);
 ini_set('display_startup_errors', 1);
 ini_set('display_errors', 1);
 date_default_timezone_set('America/New_York');
+//Iniitalize Autoloader (we're not require/require_once/include/include_once'ing
+//anything in the development environment).
 
-require dirname(__DIR__) . '/src/Tags/Base.php';
+
 
 ?>
