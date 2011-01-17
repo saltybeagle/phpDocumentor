1064d4286df659188c8aa0a034f4416d4061dc34
Shawn Stratton <sstratton@php.net>
2011-01-17 03:46:24 -0500

    Resolves Task #2 - Added autoloader to tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -7,7 +7,6 @@ ini_set('display_errors', 1);
 date_default_timezone_set('America/New_York');
 //Iniitalize Autoloader (we're not require/require_once/include/include_once'ing
 //anything in the development environment).
-
-
-
+require_once 'PEAR2/Autoload.php';
+\PEAR2\Autoload::initialize(realpath(__DIR__ . '/../src');
 ?>
